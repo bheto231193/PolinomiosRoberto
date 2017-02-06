85cd7c09bc8409f75054323d3dec076c8742efd5
bheto <javibob_stronja@hotmail.com>
2017-02-05 20:20:41 -0600

    clase de prueba

---
+++ Test.java
@@ -0,0 +1,103 @@
+package Polinomios;
+
+
+public class Test {
+
+	public static void main(String[] args) throws ListaVaciaException {
+
+		//agregar primera lista
+		Controller lista1 = new Controller();
+		System.out.printf("Lista 1");
+		lista1.insertar(5,5);
+		lista1.insertar(-1,5);
+		lista1.insertar(-3,5);
+		lista1.insertar(-4,5);
+		lista1.insertar(6,4);
+		lista1.imprimirPolinomio();
+		System.out.println("");
+		try {
+			lista1.simplificar();
+			System.out.printf("Lista 1 simplificada");
+			lista1.imprimirPolinomio();
+		} catch (ListaVaciaException e) {
+			System.out.println("\n");
+			e.printStackTrace();
+		}
+		
+
+	
+		Controller lista2 = new Controller();
+		System.out.println("");
+		System.out.printf("Lista 2");
+		lista2.insertar(5,5);
+		lista2.insertar(-1,4);
+		lista2.insertar(-3,3);
+		lista2.insertar(-4,2);
+		lista2.insertar(6,1);
+		lista2.imprimirPolinomio();
+		System.out.println("");
+		try {
+			lista2.simplificar();
+			System.out.printf("Lista 2 simplificada");
+			lista2.imprimirPolinomio();
+		} catch (ListaVaciaException e) {
+			System.out.println("\n");
+			e.printStackTrace();
+		}
+
+
+		System.out.println("");
+		System.out.println("\n------ suma---- ");
+
+		System.out.print("lista 1 ");
+		lista1.imprimirPolinomio();
+		System.out.println("");
+		System.out.printf("lista2 ");
+		lista2.imprimirPolinomio();
+		System.out.printf("\ntotal de la suma de polinomios");
+		try {
+			lista1.suma(lista2);
+		} catch (ListaVaciaException e) {
+			System.out.println("\n");
+			e.printStackTrace();
+		}
+		lista1.imprimirPolinomio();
+
+		System.out.println("");
+		System.out.println("\n------ resta---- ");
+
+		System.out.print("lista 1 ");
+		lista1.imprimirPolinomio();
+		System.out.println("");
+		System.out.printf("lista2 ");
+		lista2.imprimirPolinomio();
+		System.out.printf("\ntotal de la resta de polinomios");
+		try {
+			lista1.restarPolinomio(lista2);
+		} catch (ListaVaciaException e) {
+			System.out.println("\n");
+			e.printStackTrace();
+		}
+		lista1.imprimirPolinomio();
+		
+		
+		System.out.println("");
+		System.out.println("\n------ multiplicacion---- ");
+
+		System.out.print("lista 1 ");
+		lista1.imprimirPolinomio();
+		System.out.println("");
+		System.out.printf("lista2 ");
+		lista2.imprimirPolinomio();
+		System.out.printf("\n total de la multiplicacion de polinomios");
+		try {
+			lista1.multiplicarPolinomio(lista2);
+		} catch (ListaVaciaException e) {
+			System.out.println("\n");
+			e.printStackTrace();
+		}
+		lista1.imprimirPolinomio();
+
+	}
+
+}
