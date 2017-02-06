ffc3cce76c32a8220675a95e7f2b7f4bc0430d2f
bheto <javibob_stronja@hotmail.com>
2017-02-05 20:19:10 -0600

    clase monomio

---
+++ Monomio.java
@@ -0,0 +1,27 @@
+package Polinomios;
+
+
+public class Monomio {
+	private Integer coeficiente;
+	private Integer exponente;
+	
+	public Monomio(){
+		
+	}
+	
+	public Integer getCoeficiente() {
+		return coeficiente;
+	}
+	public void setCoeficiente(Integer coeficiente) {
+		this.coeficiente = coeficiente;
+	}
+	
+	public Integer getExponente() {
+		return exponente;
+	}
+	
+	public void setExponente(Integer exponente) {
+		this.exponente = exponente;
+	}
+	
+}
